0bf75044a590643f05291fd3286a3ebc49eeb4e5
AlbertPotter <smilepotter0128@gmail.com>
2017-04-30 22:56:00 +0300

    list pagination with tag fix

--- src/store.js
+++ src/store.js
@@ -12,6 +12,6 @@ const getMiddleware = () => {
   }
 }
 
-const store = createStore(reducer, getMiddleware(), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const store = createStore(reducer, getMiddleware());
 
 export default store;
